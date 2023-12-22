4d644d3f2e9d3147a036b081c773532922b0882f
NHAMI <nhami.habiba@gmail.com>
2023-12-22 23:12:03 +0100

    first commit

---
+++ demo4/src/test/java/emsi/ma/demo/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package emsi.ma.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
